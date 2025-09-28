a436dd13de19883deaede3f86cc4c00414fc0e1c
Aviithapa <abhishekthapa115@gmail.com>
2025-09-28 16:46:42 +0545

    destination managed

--- resources/views/website/pages/component/destination.blade.php
+++ resources/views/website/pages/component/destination.blade.php
@@ -4,22 +4,21 @@
          <div class="row justify-content-center">
              <div class="col-xl-7 col-lg-8 col-md-10">
                  <div class="section-heading text-center mb-60">
-                     <h2 class="section-title text-white mt--8 mb-25">Destinations</h2>
-                     <p class="heading-sub-txt text-white mt--1 mb--8">
-                         Top Study Destinations Around the World
-                     </p>
+                     <h2 class="section-title text-black mt--8 mb-10">Destinations</h2>
                  </div>
              </div>
          </div>
          <div class="row justify-content-center">
              @foreach ($destinations as $destination)
                  <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">
-                     <div class="facility-card facility-card-one mb-40"
+                     <div class="facility-card facility-card-one mb-40 "
                          style="background: url({{ $destination->getFirstMediaUrl('destination_image') }}) center center no-repeat;
                                 background-size: cover;
-                                background-position: top center;">
+                                background-position: top center;
+                               ">
                          <div class="facility-img-area"></div>
-                         <div class="facility-card-txt d-flex flex-column align-items-center text-center">
+                         <div class="facility-card-txt d-flex flex-column align-items-center text-center"
+                             style=" background-color: rgba(0, 0, 0, 0.4);">
                              <h3 class="facility-title"><a
                                      href="/destination/{{ $destination->slug }}">{{ $destination->title }}</a>
                              </h3>
